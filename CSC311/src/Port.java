3dd5ad6f2d490534d1fd9d862ce06cb563f7d25b
najlamohammed22 <145698408+najlamohammed22@users.noreply.github.com>
2024-02-21 17:41:53 +0300

    class

--- CSC311/src/Port.java
+++ CSC311/src/Port.java
@@ -1,4 +1,40 @@
 
 public class Port {
+	private static int calculateTotalInvestment(List<Asset> assets) { // assets list take it from Aso code 
+	    int totalInvestment = 0;
+	    for (Asset asset : assets) {
+	        totalInvestment += asset.quantity;
+	    }
+	    return totalInvestment;
+	}
+	private static double calculateExpectedReturn(List<Asset> assets, int quantity1, int quantity2, int quantity3) {
+	    double expectedReturn = 0;
+	    for (int i = 0; i < assets.size(); i++) {
+	        expectedReturn += assets.get(i).expectedReturn * calculateAllocatedQuantity(assets.get(i), quantity1, quantity2, quantity3);
+	    }
+	    return expectedReturn;
+	}
+	private static double calculatePortfolioRisk(List<Asset> assets, int quantity1, int quantity2, int quantity3) {
+	    double totalRisk = 0;
+	    for (int i = 0; i < assets.size(); i++) {
+	        totalRisk += assets.get(i).riskLevel * calculateAllocatedQuantity(assets.get(i), quantity1, quantity2, quantity3);
+	    }
+	    return totalRisk / (quantity1 + quantity2 + quantity3);
+	}
+	private static int calculateAllocatedQuantity(Asset asset, int quantity1, int quantity2, int quantity3) {
+	    switch (asset.id) {
+	        case "AAPL":
+	            return quantity1;
+	        case "GOOGL":
+	            return quantity2;
+	        case "MSFT":
+	            return quantity3;
+	        // Add more cases if there are additional assets
+	        default:
+	            throw new IllegalArgumentException("Invalid asset ID");
+	    }
+	}
+	
+	
 
 }
